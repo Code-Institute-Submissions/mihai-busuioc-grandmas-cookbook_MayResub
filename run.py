0b685b0bcb24ca0fac5470d1e309aa32497700ac
Mihai Busuioc <mihai.busuioc@freshfairy.ro>
2021-01-21 23:57:32 +0000

    added mdb, installed pymongo and dnspython

--- run.py
+++ run.py
@@ -1,15 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+     Flask, flash, render_template,
+     request, redirect, url_for, session)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello"
+@app.route("/all_receipes")
+def all_receipes():
+    receipes = mongo.db.receipes.find()
+    return render_template("receipes.html", receipes=receipes)
 
 
 if __name__ == "__main__":
