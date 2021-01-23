1e9861a58cb2faf7eb1679599d05f5c301809683
Mihai Busuioc <mihai.busuioc@freshfairy.ro>
2021-01-23 14:57:01 +0000

    Changed from mdb to materialize, implemented login, register and logout

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import (
      request, redirect, url_for, session)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import check_password_hash, generate_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,75 @@ mongo = PyMongo(app)
 def all_receipes():
     receipes = mongo.db.receipes.find()
     return render_template("receipes.html", receipes=receipes)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # Put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successfull")
+        return redirect(url_for("all_receipes"))
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if user already in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # verify hashed pass match input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] =request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(
+                        url_for("all_receipes"))
+            else:
+                # invalid pass
+                flash("Incorrect user and/or Password")
+                return redirect(url_for("login"))
+            
+        else:
+            # username doesn't exist
+            flash("Incorrect user and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/profile/<username>" , methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("all_receipes"))
+
 
 
 if __name__ == "__main__":
