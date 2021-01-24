8aded3b7361350de0ad29b5388487cc420e0325e
Mihai Busuioc <mihai.busuioc@freshfairy.ro>
2021-01-24 14:11:37 +0000

    updated the recipe how_to and ingredients maxlength back to top btn and modal rounded

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.modal').modal();
+    $('.fixed-action-btn').floatingActionButton();
+    $("a[href='#top']").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+    });
+    
   });
+
+
