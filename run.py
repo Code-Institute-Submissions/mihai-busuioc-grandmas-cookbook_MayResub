509d8ff4d24a5a906e329f0513c0ecb753afdb88
Mihai Busuioc <mihai.busuioc@freshfairy.ro>
2021-01-27 12:21:21 +0000

    run.py debug=False

--- run.py
+++ run.py
@@ -166,4 +166,4 @@ def cookbook():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
